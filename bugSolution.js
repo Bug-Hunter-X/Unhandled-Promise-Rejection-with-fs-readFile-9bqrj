d0ce82c72191d68f73c93c124bd5123e1fd7a44a
BugHunter-X <askzenai@gmail.com>
2025-02-22 03:28:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,28 @@
+```javascript
+const fs = require('fs');
+
+function readFile(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+async function processFile(filePath) {
+  try {
+    const data = await readFile(filePath);
+    // Process the data
+    console.log(data);
+  } catch (err) {
+    console.error('Error processing file:', err);
+    //Handle Error appropriately
+  }
+}
+
+processFile('nonexistent.txt'); 
+```
